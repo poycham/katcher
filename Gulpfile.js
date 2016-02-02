e48b363bb5efcb2f793517fa523b39b09dbf843c
Champ Polestico <champoyradoo@gmail.com>
2016-02-02 10:27:19 +0800

    Implement autoprefixer

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,8 @@
 var katcherNode = {
     gulp: require('gulp'),
     sass: require('gulp-sass'),
-    sourcemaps: require('gulp-sourcemaps')
+    sourcemaps: require('gulp-sourcemaps'),
+    autoprefixer: require('gulp-autoprefixer')
 };
 
 var katcherSass = {
@@ -20,6 +21,7 @@ var katcherSass = {
             .src(katcherSass.input)
             .pipe(katcherNode.sourcemaps.init())
             .pipe(katcherNode.sass(katcherSass.options)).on('error', katcherNode.sass.logError)
+            .pipe(katcherNode.autoprefixer())
             .pipe(katcherNode.sourcemaps.write(katcherSass.sourcemapPath))
             .pipe(katcherNode.gulp.dest(katcherSass.output));
     });
