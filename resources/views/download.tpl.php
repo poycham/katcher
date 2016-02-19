8e59e8976295892fef1737bccccddec9cb16d744
Champ Polestico <champoyradoo@gmail.com>
2016-02-19 19:35:51 +0800

    [Download] Download file on page load

--- resources/views/download.tpl.php
+++ resources/views/download.tpl.php
@@ -2,8 +2,14 @@
 
 <div class="row">
     <div class="col-md-5">
-        <form action="" method="POST">
-            <button type="submit" class="btn btn-primary">Download</button>
+        <form id="download-form" action="" method="POST">
+            <button type="submit" class="btn btn-primary">Download Again</button>
         </form>
     </div>
 </div>
+
+<?php $this->start('scripts') ?>
+    <script>
+        $('#download-form').submit();
+    </script>
+<?php $this->stop() ?>
--- resources/views/layouts/master.tpl.php
+++ resources/views/layouts/master.tpl.php
@@ -40,5 +40,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="<?= url('js/vendor/jquery.min.js') ?>"><\/script>')</script>
 <script src="<?= url($this->asset('js/vendor/bootstrap.min.js')) ?>"></script>
+<?= $this->section('scripts') ?>
 </body>
 </html>
